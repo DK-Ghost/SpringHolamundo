d0c1cd485de24be1a8ef373cab787367e1b92c9f
Jose Nava <dkghost117@gmail.com>
2024-06-03 22:26:55 -0600

    Inyeccion por contructores y de objetos

--- src/main/java/com/dkghost/Spring/App.java
+++ src/main/java/com/dkghost/Spring/App.java
@@ -1,19 +1,38 @@
 package com.dkghost.Spring;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.dkghost.beans.AppConfig;
+import com.dkghost.beans.AppConfig2;
 import com.dkghost.beans.Mundo;
+import com.dkghost.beans.Persona;
 
 public class App {
 
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/dkghost/xml/beans.xml");
-		Mundo m = (Mundo) appContext.getBean("mundo");
-		Mundo m2 = (Mundo) appContext.getBean("mundillo");
+		
+//		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/dkghost/xml/beans.xml");
+/*-----------Configuracion con anotaciones----------------
+		AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext();
+		appContext.register(AppConfig.class);
+		appContext.register(AppConfig2.class);
+		appContext.refresh();
+		
+		Mundo m = (Mundo) appContext.getBean("marte");
 		
 		System.out.println(m.getSaludo());
-		System.out.println(m2.getSaludo());
+----------------------------------------------------------*/
+		
+//----------------------------------Inyeccion de constructores y objetos ------------------------------------------------
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/dkghost/xml/beans.xml");
+		Persona per = (Persona) appContext.getBean("persona");	
+		System.out.println(per.getId()+" "+per.getNombre()+" "+per.getApodo()+" "+ per.getPais().getNombre()+" "+per.getPais().getCiudad().getNombre());
+		
+		((ConfigurableApplicationContext) appContext).close();
 	}
 
 }
